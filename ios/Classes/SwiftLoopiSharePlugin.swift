e7419d6bd2d05dfe7f0df1c862a02b152659f757
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2021-09-29 14:30:23 -0300

    Share video with sticker it's working

--- ios/Classes/SwiftLoopiSharePlugin.swift
+++ ios/Classes/SwiftLoopiSharePlugin.swift
@@ -3,149 +3,90 @@ import UIKit
 import PhotosUI
 
 public class SwiftLoopiSharePlugin: NSObject, FlutterPlugin {
-
-  var result: FlutterResult?
-
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "loopi_share", binaryMessenger: registrar.messenger())
-    let instance = SwiftLoopiSharePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    self.result = result
-    if(call.method == "getPlatformVersion"){
-      result("iOS " + UIDevice.current.systemVersion)
-      return 
+    
+    var result: FlutterResult?
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "loopi_share", binaryMessenger: registrar.messenger())
+        let instance = SwiftLoopiSharePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-
-    if(call.method == "shareInstagramImageStoryWithSticker"){
-      
-      let args = call.arguments as? Dictionary<String, Any>
-      shareInstagram(args: args!)
-      // let videoPath = args["videoPath"] as? String,
-      // let stickerPath = args["stickerPath"] as? String{
-      //    result(videoPath)
-      //    return
-      // }
-    }
-
-    result(FlutterMethodNotImplemented)
-
-  }
-
-    // share image via instagram stories.
-    // @ args image url
-    func shareInstagram(args:Dictionary<String,Any>)  {
-        if(args["videoPath"] == nil && args["stickerPath"] == nil){
-
-                return
-              }
-              let imageExtensions = ["png", "jpg"]
-              let instagramUrl = URL(string: "instagram-stories://share")
-              if(args["videoPath"] != nil){
-                let backgroundPath = args["videoPath"] as? String
-                let documentExists = FileManager.default.fileExists(atPath: backgroundPath!)
-                print("documentExists \(documentExists)")
-                if(documentExists){
-                  do{
-                    let url:URL? = NSURL(fileURLWithPath: backgroundPath!) as URL
-                    let pathExtension = url?.pathExtension
-                    let background = try NSData(contentsOfFile: url!.path, options: .mappedIfSafe)
-                    if(imageExtensions.contains(pathExtension!)){
-                      UIPasteboard.general.addItems([["com.instagram.sharedSticker.backgroundImage": background as Any]])
-                    }else{
-                      UIPasteboard.general.addItems([["com.instagram.sharedSticker.backgroundVideo": background as Any]])
-                    }
-                  }catch{
-                    print("Unexpected error converting to NSDATA:\(error).")
-                  }
-                }else{
-                  print("Unexpected error DOCUMENT DOES NOT EXIST.")
-                }
-              }
-
-              if(args["stickerPath"] != nil){
-                let stickerPath = args["stickerPath"] as? String
-                let documentExists = FileManager.default.fileExists(atPath: stickerPath!)
-                print("documentExists \(documentExists)")
-                if(documentExists){
-                  do{
-                    let url:URL? = URL(fileURLWithPath: stickerPath!) as URL
-                    let pathExtension = url?.pathExtension
-                    let sticker = try NSData(contentsOfFile: url!.path, options: .mappedIfSafe)
-                    if(imageExtensions.contains(pathExtension!)){
-                        do{
-                            let imageData = try Data(contentsOf: url!)
-                            let uiImage = UIImage(data: imageData)
-//                            UIPasteboard.general.addItems([["com.instagram.sharedSticker.backgroundImage": UIImagePNGRepresentation(uiImage!).pngData]])
-                        }catch {
-                            print("Error loading image : \(error)")
-                        }
-                       // UIPasteboard.general.addItems(["com.instagram.sharedSticker.stickerImage": UIImage(imageWithData: sticker) ])
-                      
-                    }
-                  }catch{
-                    print("Unexpected error converting to NSDATA:\(error).")
-                  }
-                }else{
-                  print("Unexpected error DOCUMENT DOES NOT EXIST.")
-                }
-              }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        self.result = result
         
-        if #available(iOS 10.0, *) {
-            UIApplication.shared.open(instagramUrl!)
-        } else {
-            // Fallback on earlier versions
+        if(call.method == "getPlatformVersion"){
+            result("iOS " + UIDevice.current.systemVersion)
+            return
         }
         
+        if(call.method == "shareInstagramImageStoryWithSticker"){
+            let args = call.arguments as? Dictionary<String, Any>
+            shareInstagram(args: args!)
+            return
+        }
+        
+        result(FlutterMethodNotImplemented)
+        
     }
-
-
-
-
-    // func shareInstagram(args:Dictionary<String,Any>)  {
-    //     let imageUrl=args["stickerPath"] as! String
     
-    //     let image = UIImage(named: imageUrl)
-    //     if(image==nil){
-    //         self.result!("File format not supported Please check the file.")
-    //         return;
-    //     }
-    //     guard let instagramURL = URL(string: "instagram-stories://share") else {
-    //         if let result = result {
-    //             self.result?("Instagram app is not installed on your device")
-    //             result(false)
-    //         }
-    //         return
-    //     }
+    private func shareInstagram(args:Dictionary<String,Any>)  {
+        if(args["videoPath"] == nil && args["stickerPath"] == nil){
+            self.result?("Message From Switf: videoPath or stickerPath as nil")
+            print("Message From Switf: videoPath or stickerPath as nil")
+            return
+        }
         
-    //     do{
-    //         try PHPhotoLibrary.shared().performChangesAndWait {
-    //             let request = PHAssetChangeRequest.creationRequestForAsset(from: image!)
-    //             let assetId = request.placeholderForCreatedAsset?.localIdentifier
-    //             let instShareUrl:String? = "instagram://library?LocalIdentifier=" + assetId!
-                
-    //             //Share image
-    //             if UIApplication.shared.canOpenURL(instagramURL as URL) {
-    //                 if let sharingUrl = instShareUrl {
-    //                     if let urlForRedirect = NSURL(string: sharingUrl) {
-    //                         if #available(iOS 10.0, *) {
-    //                             UIApplication.shared.open(urlForRedirect as URL, options: [:], completionHandler: nil)
-    //                         }
-    //                         else{
-    //                             UIApplication.shared.openURL(urlForRedirect as URL)
-    //                         }
-    //                     }
-    //                     self.result?("Success")
-    //                 }
-    //             } else{
-    //                 self.result?("Instagram app is not installed on your device")
-    //             }
-    //         }
         
-    //     } catch {
-    //         print("Fail")
-    //     }
-    // }
+        let instagramUrl = URL(string: "instagram-stories://share")
+        
+        guard UIApplication.shared.canOpenURL(instagramUrl! as URL) else {
+            self.result?("Instagram app is not installed on your device")
+            return
+        }
+        
+        let backgroundPath = args["videoPath"] as? String
+        let videoDocumentExists = FileManager.default.fileExists(atPath: backgroundPath!)
+        
+        let stickerPath = args["stickerPath"] as? String
+        let stickerDocumentExists = FileManager.default.fileExists(atPath: stickerPath!)
+        
+        guard videoDocumentExists && stickerDocumentExists else {
+            print("Unexpected error DOCUMENT DOES NOT EXIST.")
+            self.result?("Message From Switf: error DOCUMENT DOES NOT EXIST.")
+            return
+        }
+        
+        
+        do{
+            let url:URL? = NSURL(fileURLWithPath: backgroundPath!) as URL
+            let background = try NSData(contentsOfFile: url!.path, options: .mappedIfSafe)
+            
+            let stickerUrl:URL? = NSURL(fileURLWithPath: stickerPath!) as URL
+            let sticker = try NSData(contentsOfFile: stickerUrl!.path, options: .mappedIfSafe)
+            
+            let boardItems = ["com.instagram.sharedSticker.backgroundVideo": background as Any,
+                              "com.instagram.sharedSticker.stickerImage": sticker as Any ]
+            let pasteBoardItems = [boardItems]
+            
+            
+            guard #available(iOS 10.0, *) else {
+                UIPasteboard.general.items = pasteBoardItems
+                UIApplication.shared.openURL(instagramUrl!)
+                self.result?("Success")
+                return
+            }
+            
+            let option: [UIPasteboard.OptionsKey: Any] = [.expirationDate: Date().addingTimeInterval(60 * 5)]
+            UIPasteboard.general.setItems(pasteBoardItems, options: option)
+            UIApplication.shared.open(instagramUrl!)
+            self.result?("Success")
+            return
+        }
+        catch{
+            self.result?("Unexpected error converting to NSDATA:\(error).")
+            print("Unexpected error converting to NSDATA:\(error).")
+            
+        }
+    }
 }
