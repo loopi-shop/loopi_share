6b1d8fc3413f85e3eb0b4d6b841398603da8896e
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2021-09-29 15:15:30 -0300

    Adjusting example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:loopi_share/loopi_share.dart';
-import 'package:loopi_share_example/screens/share_image_with_sticker.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -72,6 +72,59 @@ class _MyAppState extends State<MyApp> {
             child: Text("Share image with Sticker"),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ShareImageWithSticker extends StatelessWidget {
+  const ShareImageWithSticker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ImagePicker imagePicker = ImagePicker();
+    PickedFile? backgroundVideo;
+    PickedFile? stickerImage;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Share Image Instagram With Sticker",
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                backgroundVideo = await imagePicker.getVideo(
+                  source: ImageSource.gallery,
+                );
+              },
+              child: Text("Get Video"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                stickerImage = await imagePicker.getImage(
+                  source: ImageSource.gallery,
+                );
+              },
+              child: Text("Image Sticker"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (backgroundVideo != null && stickerImage != null) {
+                  String? result = await LoopiShare.shareInstagram(
+                    videoPath: backgroundVideo!.path,
+                    stickerPath: stickerImage!.path,
+                  );
+                  print(result);
+                }
+              },
+              child: Text("Send to Instagram"),
+            ),
+          ],
+        ),
       ),
     );
   }
